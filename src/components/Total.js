07dd34b1f55a141c25e56da1b9aa85580221fd52
Devin44G <35251347+Devin44G@users.noreply.github.com>
2020-02-18 17:09:33 -0600

    Set up reducer(s) in required files -> Wrapped app in Provider tag, passing the store data to it . . .

--- src/components/Total.js
+++ src/components/Total.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-const Total = props => {
+const Total = () => {
+  const car = useSelector( state => state.car );
+  const price = useSelector( state => state.additionalPrice );
+
   return (
     <div className="content">
-      <h4>Total Amount: ${props.car.price + props.additionalPrice}</h4>
+      <h4>Total Amount: ${car.price + price}</h4>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -5,5 +5,14 @@ import App from './App';
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import featuresReducer from './reducers/featuresReducer';
+
+const store = createStore(featuresReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, rootElement);
